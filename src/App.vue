0b31a0be079fe63b747b16490b27b90bdc7c867a
seungeun <seungeun2896@naver.com>
2026-08-03 11:36:43 +0900

    commit01

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// 새로 만든 ReactiveRef 컴포넌트 불러오기
+import ReactiveRef from './components/pracitces/composition/ReactiveRef.vue'
+</script>
+
+<template>
+  <main>
+    <!-- 화면에 띄우기 -->
+    <ReactiveRef />
+  </main>
+</template>

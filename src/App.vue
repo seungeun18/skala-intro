ec0c61269875b70c1f64a0f3757599b4cdc6d14c
seungeun <seungeun2896@naver.com>
2026-08-04 14:27:23 +0900

    code challenge store

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import WeatherMockup from './components/exercise/WeatherMockup.vue'
 import WeatherComposition from './components/exercise/WeatherComposition.vue'
 import WeatherParent from './components/exercise/WeatherParent.vue'
@@ -32,15 +32,15 @@ import UnitToggler from './components/exercise/UnitToggler.vue'
       </nav>
       <main>
         <RouterView />
-        <!-- RouterView v-slot="{ Component }">
+        <RouterView v-slot="{ Component }">
           <KeepAlive>
             <component :is="Component" />
           </KeepAlive>
-        </RouterView-->
+        </RouterView>
       </main>
     </div>
   </div>
-  <!-- <div class="app-container">
+   <div class="app-container">
     <h1>⛅ 과제 5: 스토어적용</h1>
     <hr />
     <div class="dashboard-wrapper">
@@ -54,10 +54,41 @@ import UnitToggler from './components/exercise/UnitToggler.vue'
         <RouterView />
       </main>
     </div>
-  </div> -->
+  </div> 
 </template>
 
 <style>
 /* ⚠️ 외부 스타일 파일(예: 버튼 디자인 뭉치)을 이 방 안으로 쏙 가리켜 가져옵니다 */
 @import '@/assets/exercise.css';
+</style> -->
+
+<script setup>
+import StoreCounter from './components/practices/library/StoreCounter.vue'
+// import AxiosWeather from './components/practices/library/AxiosWeather.vue'
+// import AxiosJson from './components/practices/library/AxiosJson.vue'
+// import ElementPlus from './components/practices/library/ElementPlus.vue'
+// import EcmaScript from './components/practices/library/EcmaScript.vue'
+</script>
+
+<template>
+  <div class="practice-container">
+    <h1>1. Store</h1>
+    <hr />
+    <StoreCounter />
+    <h1>2. Axios</h1>
+    <hr />
+    <AxiosWeather />
+    <AxiosJson />
+    <h1>3. Element Plus</h1>
+    <hr />
+    <ElementPlus />
+    <h1>4. Modern JavaScript</h1>
+    <hr />
+    <EcmaScript />
+  </div>
+</template>
+
+<style>
+/* ⚠️ 외부 스타일 파일(예: 버튼 디자인 뭉치)을 이 방 안으로 쏙 가리켜 가져옵니다 */
+@import '@/assets/practice.css';
 </style>
